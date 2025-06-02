8bfde3a7d40fc360e67e1efdf457a43e9e43b269
captainduckie <captainduckie@users.noreply.github.com>
2025-06-02 19:20:37 -0400

    Update model.py
    Setup restnet50

--- src/model.py
+++ src/model.py
@@ -1,1 +1,11 @@
-# tbc
+import torch.nn as nn
+import torchvision.models as models
+
+class MultiLabelCNN(nn.Module):
+    def __init__(self, num_labels):
+        super().__init__()
+        self.base = models.resnet50(pretrained=True)
+        self.base.fc = nn.Linear(self.base.fc.in_features, num_labels)
+
+    def forward(self, x):
+        return self.base(x)
