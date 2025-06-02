8ed6b1e95047f2fd236ec947c02c41640d0ba5a1
captainduckie <captainduckie@users.noreply.github.com>
2025-06-02 19:21:52 -0400

    Update dataset.py
    add get_transforms() and get_dataloaders() functions

--- src/dataset.py
+++ src/dataset.py
@@ -2,3 +2,16 @@ import os
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
+
+def get_transforms():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+
+def get_dataloader(data_dir, batch_size=16):
+    transform = get_transforms()
+    dataset = ImageFolder(root=data_dir, transform=transform)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
